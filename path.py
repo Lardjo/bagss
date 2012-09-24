9b43acaec9a430db9560fd902a0179670eb80e67
Lardjo <knikitanov@me.com>
2012-09-24 22:28:13 +0400

    Minor fixes in path structure

--- path.py
+++ path.py
@@ -14,30 +14,50 @@ GamesPath = None         # Windows My Games Directory
 LocalAppPath = None      # Windows Local Data Directory 
 UserName = None          # User Name
 
-#backupDir = os.path.abspath(os.curdir + "/backup")  #Backup Directory
-
 # Get All Path
-
+#
 try:
 
     key = OpenKey(HKEY_CURRENT_USER, 'Software\\Valve\\Steam')
     SteamPath = QueryValueEx(key, 'SteamPath') [0]
     SteamGamesPath = os.path.normcase(os.path.join(SteamPath, 'steamapps', 'common'))
 
+except WindowsError:
+    pass
+
+try:
 
     key = OpenKey(HKEY_CURRENT_USER, 'Volatile Environment')
     UserPath = QueryValueEx(key, 'USERPROFILE') [0]
     DocumentsPath = os.path.normcase(os.path.join(UserPath, 'Documents'))
+
+except WindowsError:
+    pass
+
+try:
+
+    key = OpenKey(HKEY_CURRENT_USER, 'Volatile Environment')
+    UserPath = QueryValueEx(key, 'USERPROFILE') [0]
     GamesPath = os.path.normcase(os.path.join(UserPath, 'Saved Games'))
 
+except WindowsError:
+    pass
+
+try:
 
     key = OpenKey(HKEY_CURRENT_USER, 'Volatile Environment')
     LocalApp = QueryValueEx(key, 'LOCALAPPDATA') [0]
     LocalAppPath = os.path.normcase(os.path.join(LocalApp))
 
+except WindowsError:
+    pass
+
+try:
 
     key = OpenKey(HKEY_CURRENT_USER, 'Volatile Environment')
     UserName = QueryValueEx(key, 'USERNAME') [0]
 
 except WindowsError:
     pass
+#
+# END Get All Path
