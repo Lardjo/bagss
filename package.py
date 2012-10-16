58df41d8905149b5fa5431ff4dad79a15dac5115
Konstantin <knikitanov@me.com>
2012-10-16 19:55:47 +0400

    Remove unnecessary code

--- package.py
+++ package.py
@@ -5,16 +5,12 @@
 import os
 import tarfile
 import time
-
-source = None
-      
+   
 def PackageSystem(source, gname):
-
-    destination = gname
 
     try:
 
-        targetBackup = (destination + time.strftime(" (%Y.%m.%d_%H.%M.%S)") + ".tar.gz")
+        targetBackup = (gname + time.strftime(" (%Y.%m.%d_%H.%M.%S)") + ".tar.gz")
         tar = tarfile.open(os.path.join(".","backup", targetBackup), "w:gz")
         tar.add(source)
         tar.close()
