8a98a99a7e2e65ecf3634e0817d2b07a7b8b0be4
Konstantin <knikitanov@me.com>
2012-10-22 22:43:16 +0400

    Start next feature

---
+++ bagss/__init__.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+#
+# Copyright (c) 2012
+#
+# This file is part of BAGSS, and is made available under the
+# 3-clause BSD license. See LICENSE for the full details.
+
+from bagss import _version
+
+VERSION = _version.tuple
+
+__author__ = "Konstantin N."
+__homepage__ = "http://github.com/Lardjo/BAGSS"
+__version__ = _version.dotted
