4f6c3489230dded914865bf24fa81c686927c666
Konstantin <knikitanov@me.com>
2012-10-22 22:45:13 +0400

    Added new version file

---
+++ bagss/_version.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+#
+# Copyright (c) 2012
+#
+# This file is part of BAGSS, and is made available under the
+# 3-clause BSD license. See LICENSE for the full details.
+
+tuple = (0, 2, 0)
+dotted = "0.2"
+date = "2012.10.21"
+web = "bagss/0.2"
