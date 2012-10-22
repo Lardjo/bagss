8c61d8c27f22863981fce622373367e03970b03b
Konstantin <knikitanov@me.com>
2012-10-22 23:00:44 +0400

    Fix package file

--- bagss/package.py
+++ bagss/package.py
@@ -1,6 +1,9 @@
 #!usr/bin/env python3
-# File: package.py
-# Package System
+#
+# Copyright (c) 2012
+#
+# This file is part of BAGSS, and is made available under the
+# 3-clause BSD license. See LICENSE for the full details.
 
 import os
 import tarfile
